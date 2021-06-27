9db94f0bad63a74828265f5cc36fc35c325692c1
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2021-06-27 10:19:54 +0700

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,33 @@
+# (c) @AbirHasan2005
+
+import os
+
+
+class Config(object):
+	API_ID = int(os.environ.get("API_ID"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	ABOUT_BOT_TEXT = f"""
+Dapatkan link untuk setiap file yang kamu upload
+
+🧑🏻‍💻 **Developer:** @fybadmin
+"""
+	ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **Developer:** @fybadmin
+"""
+	HOME_TEXT = """
+Hi, [{}](tg://user?id={})\n\n Bot ini berguna untuk mendapatkan link dari file yang kamu upload.
+
+Kirim file dan dapatkan link untuk di share!
+"""
